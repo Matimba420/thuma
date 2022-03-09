358b0cc5d92707328df149ec20c33e28421fc6b9
Joseph <joseph.digoro@dadevs.com>
2022-03-09 10:20:55 +0200

    reg page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRegComponent } from './components/user-reg/user-reg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegComponent
   ],
   imports: [
     BrowserModule,
