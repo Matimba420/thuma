924949b91b92a06c610e8e42df67aca0e99ce72a
Joseph Digoro <joseph.digoro@dadevs.co.za>
2022-03-28 14:58:37 +0200

    login and register UI

--- frontend/thumamina/src/app/app.component.spec.ts
+++ frontend/thumamina/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'thumamina'`, () => {
+  it(`should have as title 'pass'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('thumamina');
+    expect(app.title).toEqual('pass');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('thumamina app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pass app is running!');
   });
 });
