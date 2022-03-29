95fd1c5a82291381269ddc6409bdaea934999258
Thomas Fihla <thomas@thedigitalacademy.co.za>
2022-03-29 10:40:07 +0000

    Update bdConfig.js to cloud

--- backend/db/bdConfig.js
+++ backend/db/bdConfig.js
@@ -1,10 +1,11 @@
 const  Pool  = require("pg").Pool;
 
 const pool = new Pool({
-    user: "admin",
-    host: "localhost",
-    database: "thumamina",
-    port: '4304'
+    user: "thomas",
+    password: "password123",
+    host: "129.232.211.166",
+    database: "techgeeks",
+    port: '5432'
 });
 
 module.exports = pool;
