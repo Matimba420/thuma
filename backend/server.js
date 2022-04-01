d2d94c34e48017af7affbd17fe17dda20cca2b84
ZiziphoMa <magugwanazizipho94@gmail.com>
2022-04-01 16:26:39 +0200

    fixed server

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.json());
-a
+
 app.get('/',(req,res)=>{
     res.send("Smart serve is working")
 });
