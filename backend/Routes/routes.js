4d17f9e3800d6aa79ddd5ca512db1c46a18c7779
ZiziphoMa <magugwanazizipho94@gmail.com>
2022-04-01 16:00:19 +0200

    created routes

---
+++ backend/Routes/routes.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+const controller = require('../controllers/controller')
+const router = Router();
+
+router.post('/clients', controller.addClient);
+router.get('/clients', controller.getClient);
+router.delete('/clients/:id', controller.removeClient);
+router.get('/clients/login/:email', controller.getClientByEmail);
+
+
+
+module.exports= router;
