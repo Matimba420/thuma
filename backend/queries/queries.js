6b12da0eb09a48078889937dff9c8804a83c549e
ZiziphoMa <magugwanazizipho94@gmail.com>
2022-04-04 11:59:18 +0200

    fixed db

--- backend/queries/queries.js
+++ backend/queries/queries.js
@@ -1,10 +1,10 @@
-const clientLogin = "SELECT id, firstname, lastname, cell_no, email FROM client WHERE password=$1 AND cell_no=$2";
-const addClient = "INSERT into client (name, surname, cell_no, email, password) values($1, $2, $3, $4, $5)";
-const checkClientCelllExists = "SELECT * FROM client WHERE cell_no= $3";
-const getClients = "SELECT * FROM client";
-const removeClient= "DELETE FROM client WHERE id=$1";
-const getClientById = "SELECT * FROM client WHERE id= $1"
-const getClientByEmail = "SELECT * FROM client WHERE email= $1 OR cell_no=$1"
+const clientLogin = "SELECT id, firstname, lastname, cell_no, email FROM users WHERE password=$1 AND cell_no=$2";
+const addClient = "INSERT into users (name, surname, cell_no, email, password) values($1, $2, $3, $4, $5)";
+const checkClientCelllExists = "SELECT * FROM users WHERE cell_no= $1";
+const getClients = "SELECT * FROM users";
+const removeClient= "DELETE FROM users WHERE id=$1";
+const getClientById = "SELECT * FROM users WHERE id= $1"
+const getClientByEmail = "SELECT * FROM users WHERE email= $1 OR cell_no=$1"
 
 
 
