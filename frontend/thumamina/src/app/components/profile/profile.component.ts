f1de36e8141812cfdf4e2780b7a18c030400dd13
Sanelisiwe Sogiba <sanelisiwe.sogiba@dadevs.co.za>
2022-04-05 14:19:39 +0200

    merge

--- frontend/thumamina/src/app/components/profile/profile.component.ts
+++ frontend/thumamina/src/app/components/profile/profile.component.ts
@@ -13,3 +13,28 @@ export class ProfileComponent implements OnInit {
   }
 
 }
+// export class UppassPage implements OnInit {
+//   password: any;
+//   pswUpdate: any;
+
+//   updateForm: any;
+
+//   constructor(private formBuilder: FormBuilder, private service: UpdateService) { }
+
+//   ngOnInit():void {
+    
+// this.updatePass();
+//     this.password=this.formBuilder.group({
+//       pwd:new FormControl()
+//     })
+//   };
+
+//   updatePass(){
+//     this.service.updatePass(this.password.value).subscribe(
+//       (data: any) => {
+//         this.pswUpdate = data.results;
+//       }
+//     )
+//   }
+// }
+
