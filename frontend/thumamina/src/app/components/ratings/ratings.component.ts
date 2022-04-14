434922ab19e9857a99fe23d5a27c54e765bb174e
ZiziphoMa <zizipho.magugwana@dadevs.co.za>
2022-04-14 13:03:31 +0200

    ratings

--- frontend/thumamina/src/app/components/ratings/ratings.component.ts
+++ frontend/thumamina/src/app/components/ratings/ratings.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-ratings',
   templateUrl: './ratings.component.html',
-  styleUrls: ['./ratings.component.css']
+  styleUrls: ['./ratings.component.css'],
+
 })
 export class RatingsComponent implements OnInit {
+  ratings: { client: string; errand: string; address: string; comment: string; } | any;
+
+  rating3: any
 
   constructor() { }
 
   ngOnInit(): void {
+
+
+    this.ratings={
+      client: "Zizi Zozi",
+      errand: "Shopping",
+      address: "44 Stanely Avenue, Braamfontein, Johannesburg, 2001",
+      comment: "The service was great and my dress fits me perfecly thank you.I recommend Zizi to everyone that wanna do shooping."
+    }
 
     
   }
