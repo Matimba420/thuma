79c6f5f4913763b35c9730c0befe2306545bf005
Joseph Digoro <joseph.digoro@dadevs.co.za>
2022-04-21 15:00:51 +0200

    added env

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,14 @@ require('dotenv').config();
 const  Pool  = require("pg").Pool;
 const connectionString=`postgresql://${process.env.DB_USER}/${process.env.DB_PWD}/@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 const pool = new Pool({
-    connectionString: process.env.NODE_ENV=="production"? process.env.DB_URL : connectionString
+    // connectionString: process.env.NODE_ENV=="production"? process.env.DB_URL : connectionString
+    
+            user: process.env.DB_USER,
+            host:process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT,
+            password: process.env.DB_PWD
+        
 }); 
 
 module.exports = pool;
