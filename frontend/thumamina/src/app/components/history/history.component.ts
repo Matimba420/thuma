85c5119ef459f2f753801d8dc0c414c21fb3c6b5
Kelebogil <95358859+Kelebogil@users.noreply.github.com>
2022-04-21 16:02:24 +0200

    user history

--- frontend/thumamina/src/app/components/history/history.component.ts
+++ frontend/thumamina/src/app/components/history/history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Service } from 'src/app/interface/service';
 
 @Component({
   selector: 'app-history',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryComponent implements OnInit {
 
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
 
-}
+     
+    }
+  }
+  
+
